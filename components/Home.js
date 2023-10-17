a99c0754bccba67e1cc62cbe6cf6cfee76f6f9ea
Roger-nguyen1 <roger.nguyen@hotmail.fr>
2023-10-17 13:07:43 +0200

    Home modified

--- components/Home.js
+++ components/Home.js
@@ -1,7 +1,20 @@
 import styles from "../styles/Home.module.css";
+import { useState } from "react";
+import { searchCity } from "../modules/searchCity";
+import { searchCoordinates } from "../modules/searchCoordinates";
 import NavBar from "../components/NavBar";
 
 function Home() {
+  const [inputSearch, setInputSearch] = useState("");
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      // La touche "Entrée" a été pressée, vous pouvez exécuter votre fonction ici.
+      // Par exemple, vous pouvez appeler une fonction de recherche ou effectuer une action souhaitée.
+      console.log("Touche Entrée pressée");
+    }
+  };
+
   return (
     <div data-theme="night">
       <NavBar />
@@ -10,12 +23,16 @@ function Home() {
         <div>
           <input
             type="text"
-            placeholder="Type ici"
+            placeholder="Rechercher une station"
             className="input input-bordered input-info w-full max-w-xs"
+            id="inputSearch"
+            onChange={(e) => setInputSearch(e.target.value)}
+            value={inputSearch}
+            onKeyDown={handleKeyPress}
           />
 
           <p>du texte PAS ICI</p>
-          <button className="btn btn-accent">Accent</button>
+          <button className="btn btn-accent">Rechercher</button>
         </div>
       </main>
     </div>
