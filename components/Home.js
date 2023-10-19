8a3f1ce5fff68f73ec0c7b243ab5ac1f36e31e40
Roger-nguyen1 <roger.nguyen@hotmail.fr>
2023-10-19 12:13:14 +0200

    searchCity Modified

--- components/Home.js
+++ components/Home.js
@@ -2,17 +2,39 @@ import styles from "../styles/Home.module.css";
 import { useState } from "react";
 import { searchCity } from "../modules/searchCity";
 import { searchCoordinates } from "../modules/searchCoordinates";
+import Station from "../components/Station";
 import NavBar from "../components/NavBar";
 
 function Home() {
   const [inputSearch, setInputSearch] = useState("");
+  const [stationsData, setStationsData] = useState(null);
 
   const handleKeyPress = (e) => {
     if (e.key === "Enter") {
       const result = searchCity(inputSearch);
       console.log("Touche Entrée pressée");
+      if (result) {
+        setStationsData(result);
+        console.log(stationsData);
+      }
     }
   };
+
+  //mise à jour des stations avec stationsData.map
+  const stations = stationsData.map((data, i) => {
+    return (
+      <Station
+        key={i}
+        updateLikedMovies={updateLikedMovies}
+        isLiked={isLiked}
+        title={data.title}
+        overview={tronquerTexte(data.overview, 250)}
+        poster={`${posterUrl}${data.poster_path}`}
+        voteAverage={data.vote_average}
+        voteCount={data.vote_count}
+      />
+    );
+  });
 
   return (
     <div data-theme="night">
@@ -28,11 +50,13 @@ function Home() {
             onChange={(e) => setInputSearch(e.target.value)}
             value={inputSearch}
             onKeyDown={handleKeyPress}
+            required
           />
 
           <p>du texte PAS ICI</p>
           <button className="btn btn-accent">Rechercher</button>
         </div>
+        {/* <div>{stations}</div> */}
       </main>
     </div>
   );
--- modules/searchCity.js
+++ modules/searchCity.js
@@ -1,66 +1,40 @@
 export function searchCity(city) {
-  const apiCarburant = "https://api.prix-carburants.2aaz.fr/stations/?q=";
-  let newDatas;
-  let newDatasWithPrice;
+  const apiStations = "https://api.prix-carburants.2aaz.fr/stations/?q=";
+  const apiById = `https://api.prix-carburants.2aaz.fr/station/`;
+  let arrayOfId;
+  let fetchId = false;
+  const newDatasWithPrice = [];
 
-  //Fonction de recherche des prix Fuels par station.id
-  async function fetchStationDataById(stationId) {
-    const apiUrl = `https://api.prix-carburants.2aaz.fr/station/${stationId}`;
-    try {
-      const response = await fetch(apiUrl);
-      if (response.ok) {
-        const data = await response.json();
-        return data;
+  //Recherche des stations avec création d'un tableaux d'id, et recherche des prix des carburants
+  fetch(apiStations + city)
+    .then((response) => response.json())
+    .then((data) => {
+      arrayOfId = data.map((item) => item.id);
+      fetchId = data !== null;
+      //console.log(newDatas);
+
+      //Requêtes par id et créations des données des stations
+      if (fetchId) {
+        for (let i = 0; i < arrayOfId.length; i++) {
+          fetch(apiById + arrayOfId[i])
+            .then((response) => response.json())
+            .then((data) => {
+              const dataModified = {
+                id: data.id,
+                Brand: data.Brand.name,
+                name: data.name,
+                Fuels: data.Fuels,
+              };
+              //console.log(dataModified);
+              newDatasWithPrice.push(dataModified);
+              //console.log(newDatasWithPrice);
+            });
+        }
       }
-      return null;
-    } catch (error) {
-      console.error(
-        "Erreur lors de la récupération des données de la station : ",
-        error
-      );
-      return null;
-    }
-  }
+    })
+    .catch((error) => console.error("Erreur:", error));
 
-  //Récupération des données de la station par son id et ajout des prix Fuels aux données
-  async function enrichDataWithFuelPrices() {
-    for (const station of newDatas) {
-      const stationData = await fetchStationDataById(station.id);
-      if (stationData) {
-        newDatasWithPrice.push({
-          id: station.id,
-          Brand: {
-            name: station.Brand.name,
-          },
-          Address: station.Address,
-          Coordinates: station.Coordinates,
-          Prices: stationData.Fuels, // Ajout des prix des carburants
-        });
-      }
-    }
-  }
+  //console.log(newDatasWithPrice);
 
-  //Recherche de la station et création nouveau tableaux d'objets
-  if (city) {
-    fetch(apiCarburant + city)
-      .then((response) => response.json())
-      .then((data) => {
-        newDatas = data.map((item) => ({
-          id: item.id,
-          name: item.name,
-          Brand: {
-            name: item.Brand.name,
-          },
-          Address: item.Address,
-          Coordinates: item.Coordinates,
-        }));
-        console.log(newDatas);
-      })
-      .catch((error) => console.error("Erreur:", error));
-
-    console.log(newDatasWithPrice);
-  }
-
-  // enrichDataWithFuelPrices();
   return newDatasWithPrice;
 }
